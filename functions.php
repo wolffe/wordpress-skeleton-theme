a5b328e730426d5dc8fc8c46306b80677131ea2f
Ciprian Popescu <wolffe@users.noreply.github.com>
2017-09-29 16:31:40 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,51 @@
 <?php
 add_theme_support('post-thumbnails');
 
-register_sidebar(array(
-	'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	'after_widget' => '</div>',
-	'before_title' => '<h2 class="side-title">',
-	'after_title' => '</h2>',
-));
+function whiskey_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('custom-logo');
+    add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption'));
+
+    register_nav_menus( array(
+        'primary'  => __('Primary Menu', 'whiskey'),
+        'secondary_bottom'  => __('Secondary Menu - Bottom', 'whiskey'),
+        'bottom' => __('Bottom Menu', 'whiskey'),
+        'primary_pipes'  => __('Primary Pipes Menu', 'whiskey'),
+    ));
+
+    register_sidebar(array(
+        'name' => __('Right Widget Area', 'whiskey' ),
+        'id' => 'right-widget-area',
+        'description' => __( 'The right widget area', 'whiskey' ),
+        'before_widget' => '<div class="sidebox">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widgettitle">',
+        'after_title' => '</h3>',
+    ));
+
+    register_sidebar(array(
+        'name' => __('Bottom Widget Area', 'whiskey' ),
+        'id' => 'bottom-widget-area',
+        'description' => __( 'The bottom widget area', 'whiskey' ),
+        'before_widget' => '<div class="bottom-box">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widgettitle">',
+        'after_title' => '</h3>',
+    ));
+}
+
+add_action('after_setup_theme', 'whiskey_setup');
+
+function whiskey_load_assets() {
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('whiskey', 'path/to/js', array('jquery'), '1.0.0', true);
+
+    wp_enqueue_style('googlefonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,700');
+    wp_enqueue_style('whiskey', 'path/to/css');
+}
+add_action('wp_enqueue_scripts', 'whiskey_load_assets');
+
+if (!isset($content_width)) {
+    $content_width = 1024;
+}
